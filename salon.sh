858781568909821584d71164a067782272d8c4ee
KailyLuna <89271710+KailyLuna@users.noreply.github.com>
2024-02-13 16:47:47 -0700

    Update salon.sh
    Added more to the service menu function

--- salon.sh
+++ salon.sh
@@ -29,6 +29,47 @@ SERVICE_MENU() {
 
     # then redisplay services offered
     SERVICE_MENU
+  else 
+    # if their input was correct, ask for their number 
+    echo -e "\nWhat's your phone number?"
+
+    # reading the user input for a phone number
+    read CUSTOMER_PHONE
+
+    # SQL query for retrieving the name from customers table
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # if the customer does NOT exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      # tell user their name isn't in database 
+      echo -e "\nI don't have a record for you. Let us put you in the database."
+      
+      # get new customer name
+      echo "What's your name?"
+      read CUSTOMER_NAME
+      
+      # insert new customer into customers table
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    fi
+
+    # get service id info
+    SERVICE_INFO=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+    SERVICE_INFO_FORMATTED=$(echo $SERVICE_INFO | sed 's/ |/"/')
+
+    # ask the user what time they would like their appointment scheduled
+    echo -e "\nWhat time would you like your "$SERVICE_INFO_FORMATTED"?"
+
+    # read input from user
+    read SERVICE_TIME
+
+    # get customer_id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # insert the data into appointments table
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(service_id, customer_id, time) VALUES('$SERVICE_ID_SELECTED','$CUSTOMER_ID','$SERVICE_TIME')")
+
+    echo -e "\nI have put you down for the $SERVICE_INFO_FORMATTED, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
   fi
 
 }
